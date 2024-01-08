9c945939897d5b137a269ec938d65c18a4745877
BenPotel <benjamin.potel@proton.me>
2024-01-08 10:52:15 +0100

    Working Routes (no css yet)

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/Kasa.svg";
+
+function About() {
+  return (
+    <div>
+      <img src={logo} alt="Logo Kasa" />
+      <h1>KASA À propos</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/Kasa.svg";
+
+function Home() {
+  return (
+    <div>
+      <img src={logo} alt="Logo Kasa" />
+      <h1>KASA TEST</h1>
+    </div>
+  );
+}
+
+export default Home;
